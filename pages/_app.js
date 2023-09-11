af9e8f8b153940b41b2823e74affb13b99386468
gxg <gxg@pop-os.localdomain>
2023-09-11 12:37:26 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,12 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import TagManager from "react-gtm-module";
 import "styles/style.scss";
+import Script from "next/script";
+import * as gtag from "./gtag";
+import { useRouter } from "next/router";
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
   // default theme setup
   const { default_theme } = config.settings;
 
@@ -16,12 +20,20 @@ const App = ({ Component, pageProps }) => {
   const sf = theme.fonts.font_family.secondary;
   const [fontcss, setFontcss] = useState();
   useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
     fetch(
       `https://fonts.googleapis.com/css2?family=${pf}${
         sf ? "&family=" + sf : ""
       }&display=swap`
     ).then((res) => res.text().then((css) => setFontcss(css)));
-  }, [pf, sf]);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [pf, sf, router.events]);
 
   // google tag manager (gtm)
   const tagManagerArgs = {
@@ -38,6 +50,24 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <JsonContext>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         {/* google font css */}
         <link
